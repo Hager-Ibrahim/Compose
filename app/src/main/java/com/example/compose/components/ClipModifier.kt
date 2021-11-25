cf2c730306ff5a77d90830e51e88b1e343d035ef
Hager_Ibrahim <hager.ibrahim@trianglz.com>
2021-11-25 15:40:13 +0200

    add ClipModifier

---
+++ app/src/main/java/com/example/compose/components/ClipModifier.kt
@@ -0,0 +1,28 @@
+package com.example.compose.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun ClipModifier() {
+    Text(
+        text = "Text with Clipped background",
+        color = Color.White,
+        modifier = Modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .padding(10.dp)
+            .clip(RoundedCornerShape(25.dp))
+            .background(Color.Blue)
+            .padding(16.dp)
+    )
+}
