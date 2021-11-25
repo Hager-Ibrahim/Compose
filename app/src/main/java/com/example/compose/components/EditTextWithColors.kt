5163d037e2ef9a32fed03d66282b7040609bf83c
Hager_Ibrahim <hager.ibrahim@trianglz.com>
2021-11-25 15:13:34 +0200

    add ediitext example with modifier

---
+++ app/src/main/java/com/example/compose/components/EditTextWithColors.kt
@@ -0,0 +1,28 @@
+package com.example.compose.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EditTextExample() {
+    TextField(
+        value = "", onValueChange = {},
+        Modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .padding(24.dp),
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFF354b60),
+            backgroundColor = Color(0xFFe1e5e9),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        )
+    )
+}
--- app/src/main/java/com/example/compose/components/MainFragment.kt
+++ app/src/main/java/com/example/compose/components/MainFragment.kt
@@ -7,9 +7,17 @@ import android.view.ViewGroup
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
 import androidx.fragment.app.Fragment
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainFragment : Fragment() {
 
@@ -24,11 +32,15 @@ class MainFragment : Fragment() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    EditTextExample()
                 }
             }
         }
     }
 
+    @Composable
+    fun EditTextExample() {
+        
+    }
 
 }
