89925ca6d4d08e129dfb7eb98b347b23ec12fa42
Hager_Ibrahim <hager.ibrahim@trianglz.com>
2022-02-24 15:59:18 +0200

    Try single event in compose

--- app/src/main/java/com/example/compose/modules/main/MainActivity.kt
+++ app/src/main/java/com/example/compose/modules/main/MainActivity.kt
@@ -4,11 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
-import com.example.compose.ui.navigationexamples.seventh.SetupNavGraph
+import com.example.compose.ui.navigationexamples.eighth.SetupNavGraph
 import com.example.compose.ui.theme.ComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
