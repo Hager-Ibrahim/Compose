8b3647342197897fb4f6de886faaa9f7f3494212
Hager_Ibrahim <hager.ibrahim@trianglz.com>
2022-03-24 15:31:47 +0200

    Add StaffContent

--- app/src/main/java/com/example/compose/modules/staff/examples/f/UpdateList.kt
+++ app/src/main/java/com/example/compose/modules/staff/examples/f/UpdateList.kt
@@ -7,35 +7,42 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.compose.modules.staff.StaffViewModel
 import com.example.compose.modules.staff.model.StaffUIModel
+import com.example.compose.modules.staff.screen.StaffCard
 import com.example.compose.modules.staff.screen.StaffItem
 
 @Composable
 fun UpdateStaff(viewModel: StaffViewModel) {
-    val staffList = viewModel.staffList
-    val updateStaff: ((Int) -> Unit)= { viewModel.update(it) }
+    val staffList = remember{viewModel.staffList}
 
+
+    StaffContent(onClick = {
+          viewModel.update(it)
+    }, staff = staffList)
+
+}
+
+@Composable
+fun StaffContent(onClick: (Int) -> Unit, staff: List<StaffUIModel>){
     LazyColumn(
         contentPadding = PaddingValues(12.dp)
     ) {
 
-        staffList.forEachIndexed { index, staff ->
+        staff.forEachIndexed { index, staff ->
             item {
                 StaffCard(staff = staff, index){
-                    updateStaff(index)
+                   onClick(index)
                 }
             }
         }
     }
 }
-
 @Composable
 fun StaffCard(staff: StaffUIModel,
               position: Int,
