c14f53dc369cb652e7dcdd4792a587bbc80c2f35
Hager_Ibrahim <hager.ibrahim@trianglz.com>
2022-03-27 15:42:38 +0200

    Add advanced example

--- app/src/main/java/com/example/compose/modules/login/stateexamples/j/LoginScreen.kt
+++ app/src/main/java/com/example/compose/modules/login/stateexamples/j/LoginScreen.kt
@@ -1,11 +1,11 @@
-package com.example.compose.modules.login.stateexamples.i
+package com.example.compose.modules.login.stateexamples.j
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,37 +15,49 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.compose.R
+import com.example.compose.modules.login.stateexamples.i.ErrorIcon
 import com.example.compose.ui.theme.DarkGrey
 import com.example.compose.ui.theme.LightGrey
 
 @Composable
 fun LoginScreen(viewModel: LoginViewModel) {
-    val text = viewModel.phone.observeAsState()
+    val text = viewModel.email.observeAsState()
+    var passwordVisibility by remember{ mutableStateOf(false)}
 
-    NormalTextField(
+    PasswordTextField(
         text = text.value ,
-        stringResource(id = R.string.phone)
-    ) {
-        viewModel.updatePhone(it)
-    }
+        stringResource(id = R.string.phone),
+        passwordVisibility,{
+        viewModel.updateEmail(it)},{
+            passwordVisibility = !passwordVisibility
+        })
 
 }
 
 @Composable
-fun NormalTextField(
+fun PasswordTextField(
     text: String?,
     hint: String? = "",
+    isPasswordVisible: Boolean,
     onValueChanged: (String) -> Unit,
+    onPasswordIconClicked: () -> Unit
 
     ) {
     val borderColor = if (text?.isEmpty() == true) Color.Red else Color.Transparent
-    val isShowErrorIcon = text?.isEmpty() == true
+    val passwordIcon = if(isPasswordVisible) R.drawable.ic_visibility_on else R.drawable.ic_visibility_off
+    val visualTransformation =  if(isPasswordVisible)
+        PasswordVisualTransformation()
+    else VisualTransformation.None
+
+
 
     TextField(
         value = text ?:"",
@@ -67,59 +79,41 @@ fun NormalTextField(
             unfocusedIndicatorColor = Color.Transparent,
         ), placeholder = {
             Text(hint ?: "")
-        },leadingIcon={
-            CountryCode()
         }, trailingIcon = {
-            if(isShowErrorIcon) ErrorIcon()
+            PasswordIcon(icon = passwordIcon){
+                onPasswordIconClicked()
+            }
         }
         , keyboardOptions = KeyboardOptions(
-            keyboardType= KeyboardType.Phone,
+            keyboardType= KeyboardType.Password,
             imeAction = ImeAction.Done)
-    )
+    , visualTransformation = visualTransformation)
 }
 
 @Composable
-fun CountryCode(){
-    Row(  modifier =
-    Modifier
-        .padding(horizontal = 16.dp,)
-        .clip(RoundedCornerShape(16.dp))
-        .background(Color.White)
-        .padding(horizontal = 4.dp, vertical = 8.dp)
-    ) {
-        Text(
-            text = "+20",
-            modifier = Modifier.align(
-                Alignment.CenterVertically))
-        Image(
-            modifier =
-            Modifier.padding(horizontal = 6.dp),
-            painter = painterResource(id = R.drawable.eg),
-            contentDescription = "",
+fun PasswordIcon(icon: Int, onPasswordIconClicked: () -> Unit){
+        Icon(
+            painterResource(id = icon),
+            contentDescription = null,
+            modifier = Modifier.clickable {
+                onPasswordIconClicked()
+            }
         )
-    }
-}
-@Composable
-fun ErrorIcon(){
-    Image(
-        painterResource(id = R.drawable.ic_error),
-        contentDescription = null,
-    )
 }
 class LoginViewModel : ViewModel() {
 
 
-    private val _phone = MutableLiveData<String>()
-    val phone: LiveData<String>
-        get() = _phone
+    private val _email = MutableLiveData<String>()
+    val email: LiveData<String>
+        get() = _email
 
-    fun updatePhone(newValue: String) {
-        _phone.value = newValue
+    fun updateEmail(newValue: String) {
+        _email.value = newValue
     }
 }
 
 @Preview
 @Composable
-fun PreviewNormalEditText(){
-    NormalTextField(text = "", onValueChanged = {})
+fun PreviewPasswordEditText(){
+    PasswordTextField(text = "","", true, {  },{})
 }
