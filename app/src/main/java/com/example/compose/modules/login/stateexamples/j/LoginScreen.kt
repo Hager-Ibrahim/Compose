66be79f1547841d4b2a35758a0f0ef405d60ae78
Hager_Ibrahim <hager.ibrahim@trianglz.com>
2022-04-03 16:37:17 +0200

    Remove redundant files

--- app/src/main/java/com/example/compose/modules/login/stateexamples/j/LoginScreen.kt
+++ app/src/main/java/com/example/compose/modules/login/stateexamples/j/LoginScreen.kt
@@ -1,29 +1,15 @@
 package com.example.compose.modules.login.stateexamples.j
 
 
-import android.util.Log
-import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.*
 import com.example.compose.R
-import com.example.compose.modules.login.stateexamples.i.NormalTextField
-import com.example.compose.ui.theme.DarkGrey
-import com.example.compose.ui.theme.LightGrey
+import com.example.compose.ui.commons.textField.NormalTextField
+import com.example.compose.ui.commons.textField.PasswordTextField
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
@@ -61,66 +47,6 @@ fun LoginScreen(viewModel: LoginViewModel) {
 }
 
 
-@Composable
-fun PasswordTextField(
-    text: String?,
-    hint: String? = "",
-    isPasswordVisible: Boolean,
-    onValueChanged: (String) -> Unit,
-    onPasswordIconClicked: () -> Unit
-
-) {
-
-    val borderColor = if (text?.isEmpty() == true) Color.Red else Color.Transparent
-    val passwordIcon =
-        if (isPasswordVisible) R.drawable.ic_visibility_on else R.drawable.ic_visibility_off
-    val visualTransformation = if (isPasswordVisible)
-        PasswordVisualTransformation()
-    else VisualTransformation.None
-
-
-
-    TextField(
-        value = text ?: "",
-        onValueChange = {
-            onValueChanged(it)
-        },
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(16.dp)
-            .clip(RoundedCornerShape(16.dp))
-            .border(
-                1.dp, borderColor, RoundedCornerShape(16.dp)
-            ),
-        colors = TextFieldDefaults.textFieldColors(
-            textColor = DarkGrey,
-            backgroundColor = LightGrey,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-        ), placeholder = {
-            Text(hint ?: "")
-        }, trailingIcon = {
-            PasswordIcon(icon = passwordIcon) {
-                onPasswordIconClicked()
-            }
-        }, keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Password,
-            imeAction = ImeAction.Done
-        ), visualTransformation = visualTransformation
-    )
-}
-
-@Composable
-fun PasswordIcon(icon: Int, onPasswordIconClicked: () -> Unit) {
-    Icon(
-        painterResource(id = icon),
-        contentDescription = null,
-        modifier = Modifier.clickable {
-            onPasswordIconClicked()
-        }
-    )
-}
 
 class LoginViewModel : ViewModel() {
 
--- app/src/main/java/com/example/compose/ui/commons/textField/NormalTextField.kt
+++ app/src/main/java/com/example/compose/ui/commons/textField/NormalTextField.kt
@@ -1,41 +1,31 @@
-package com.example.compose.modules.login.stateexamples.i
+package com.example.compose.ui.commons.textField
 
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.compose.R
 import com.example.compose.ui.theme.DarkGrey
 import com.example.compose.ui.theme.LightGrey
 
-@Composable
-fun LoginScreen(viewModel: LoginViewModel) {
-    val text = viewModel.email.observeAsState()
-
-    NormalTextField(
-        text = text.value ,
-        stringResource(id = R.string.phone)
-    ) {
-        viewModel.updateEmail(it)
-    }
-
-}
 
 @Composable
 fun NormalTextField(
@@ -70,7 +60,7 @@ fun NormalTextField(
         },leadingIcon={
             CountryCode()
         }, trailingIcon = {
-            if(isErrorIconVisible) ErrorIcon(text)
+            if(isErrorIconVisible) ErrorIcon()
         }
         , keyboardOptions = KeyboardOptions(
             keyboardType= KeyboardType.Text,
@@ -100,7 +90,7 @@ fun CountryCode(){
     }
 }
 @Composable
-fun ErrorIcon(text: String?){
+fun ErrorIcon(){
     Image(
         painterResource(id = R.drawable.ic_error),
         contentDescription = null,
@@ -108,21 +98,4 @@ fun ErrorIcon(text: String?){
 
         }
     )
-}
-class LoginViewModel : ViewModel() {
-
-
-    private val _email = MutableLiveData<String>()
-    val email: LiveData<String>
-        get() = _email
-
-    fun updateEmail(newValue: String) {
-        _email.value = newValue
-    }
-}
-
-@Preview
-@Composable
-fun PreviewNormalEditText(){
-    NormalTextField(text = "", onValueChanged = {})
 }
