91c15d306ff5c7108df8d11d6f77a6b1820fdc1a
Hager_Ibrahim <hager.ibrahim@trianglz.com>
2022-05-16 14:03:27 +0200

    Add NavHostController functions to NavigationExtension.kt

--- app/src/main/java/com/example/compose/modules/main/MainScreen.kt
+++ app/src/main/java/com/example/compose/modules/main/MainScreen.kt
@@ -9,6 +9,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.*
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.compose.ui.common.extentions.findStartDestinationId
+import com.example.compose.ui.common.extentions.getCurrentDestinationRouteAsState
+import com.example.compose.ui.common.extentions.shouldShowBottomBar
 import com.example.compose.ui.navigation.model.BottomBarScreen
 
 @Composable
@@ -25,15 +28,12 @@ fun MainScreen(navController: NavHostController) {
         floatingActionButton = {
 
         }
-    ) {
-        SetupMainNavHost(navController)
-    }
+    ) { SetupMainNavHost(navController) }
 }
 
 @Composable
 fun BottomBar(navController: NavHostController) {
     val screens = BottomBarScreen.values()
-
     val currentDestinationRoute = navController.getCurrentDestinationRouteAsState()
 
 //    Row{
@@ -86,17 +86,4 @@ fun RowScope.AddItem(
     )
 }
 
-val bottomBarTabs = BottomBarScreen.values()
-private val bottomBarRoutes = bottomBarTabs.map { it.route }
-
-@Composable
-fun NavHostController.getCurrentDestinationRouteAsState() =
-    currentBackStackEntryAsState().value?.destination?.route
-
-@Composable
-fun NavHostController.shouldShowBottomBar(): Boolean {
-    return getCurrentDestinationRouteAsState() in bottomBarRoutes
-}
-
-fun NavHostController.findStartDestinationId() = graph.findStartDestination().id
 
--- app/src/main/java/com/example/compose/ui/common/extentions/NavigationExtension.kt
+++ app/src/main/java/com/example/compose/ui/common/extentions/NavigationExtension.kt
@@ -1,9 +1,23 @@
 package com.example.compose.ui.common.extentions
 
+import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.compose.ui.navigation.model.BottomBarScreen
 
-fun NavController.navigateAndReplaceStartRoute(newRoute: String) {
-    popBackStack(graph.startDestinationId, true)
-    graph.setStartDestination(newRoute)
-    navigate(newRoute)
+val bottomBarTabs = BottomBarScreen.values()
+private val bottomBarRoutes = bottomBarTabs.map { it.route }
+
+@Composable
+fun NavHostController.getCurrentDestinationRouteAsState() =
+    currentBackStackEntryAsState().value?.destination?.route
+
+@Composable
+fun NavHostController.shouldShowBottomBar(): Boolean {
+    return getCurrentDestinationRouteAsState() in bottomBarRoutes
 }
+
+fun NavHostController.findStartDestinationId() = graph.findStartDestination().id
+
